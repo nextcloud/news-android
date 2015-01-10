62ba675db1ea3afa10ebcbfbe38e275307eea5d9
David Development <david-dev@live.de>
2015-01-10 19:13:27 +0100

    Add option menu for long pressing a link - #293

--- News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/interfaces/WebViewLinkLongClickInterface.java
+++ News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/interfaces/WebViewLinkLongClickInterface.java
@@ -1,9 +1,17 @@
 package de.luhmer.owncloudnewsreader.interfaces;
 
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.ClipData;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.webkit.JavascriptInterface;
+import android.widget.Toast;
+
+import de.luhmer.owncloudnewsreader.services.PodcastDownloadService;
 
 /**
  * Created by David on 25.03.2014.
@@ -18,11 +26,47 @@ public class WebViewLinkLongClickInterface {
 
     /** Show a toast from the web page */
     @JavascriptInterface
-    public void openLinkInBrowser(String url) {
-        if (!url.startsWith("http://") && !url.startsWith("https://"))
-            url = "http://" + url;
+    public void openLinkInBrowser(String urlTemp) {
+        if (!urlTemp.startsWith("http://") && !urlTemp.startsWith("https://"))
+            urlTemp = "http://" + urlTemp;
 
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
-        mContext.startActivity(browserIntent);
+        final String url = urlTemp;
+
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(mContext)
+                .setNegativeButton("Abort", null)
+                .setNeutralButton("Open in browser", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                        mContext.startActivity(browserIntent);
+                    }
+                })
+                .setTitle("Link")
+                .setMessage("Choose an option below");
+
+
+
+
+        alertDialog.setPositiveButton("Copy link", new DialogInterface.OnClickListener() {
+            @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                int sdk = android.os.Build.VERSION.SDK_INT;
+                if(sdk < android.os.Build.VERSION_CODES.HONEYCOMB) {
+                    android.text.ClipboardManager clipboard = (android.text.ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+                    clipboard.setText(url);
+                } else {
+                    android.content.ClipboardManager clipboard = (android.content.ClipboardManager) mContext.getSystemService(Context.CLIPBOARD_SERVICE);
+                    android.content.ClipData clip = android.content.ClipData.newPlainText(url, url);
+                    clipboard.setPrimaryClip(clip);
+                }
+            }
+        });
+
+
+        alertDialog.show();
+
+        //Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        //mContext.startActivity(browserIntent);
     }
 }
