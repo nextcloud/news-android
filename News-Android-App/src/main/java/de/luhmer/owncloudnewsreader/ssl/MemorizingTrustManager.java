6f36801a3e53bb0c686ef78dd905983648654de1
Daniel Schaal <daniel@schaal.email>
2015-07-08 06:53:02 +0200

    Check if keystore file is readable

--- News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/ssl/MemorizingTrustManager.java
+++ News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/ssl/MemorizingTrustManager.java
@@ -220,9 +220,9 @@ public class MemorizingTrustManager implements X509TrustManager {
 		}
 		try {
 			ks.load(null, null);
-			ks.load(new java.io.FileInputStream(keyStoreFile), "MTM".toCharArray());
-		} catch (java.io.FileNotFoundException e) {
-			Log.i(TAG, "getAppKeyStore(" + keyStoreFile + ") - file does not exist");
+			if(keyStoreFile.canRead()) {
+				ks.load(new java.io.FileInputStream(keyStoreFile), "MTM".toCharArray());
+			}
 		} catch (Exception e) {
 			Log.e(TAG, "getAppKeyStore(" + keyStoreFile + ")", e);
 		}
