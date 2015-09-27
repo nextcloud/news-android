76e693c40e8d3de8fbb646e4877065675747d3a9
Daniel Schaal <daniel@schaal.email>
2015-09-27 09:53:25 +0200

    Add parameters to AsyncTaskHelper, remove unused method

--- News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/helper/AsyncTaskHelper.java
+++ News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/helper/AsyncTaskHelper.java
@@ -7,16 +7,8 @@ import android.os.Build;
  * Created by David on 20.07.2015.
  */
 public class AsyncTaskHelper {
-
-    public static <T> void StartAsyncTask(AsyncTask asyncTask, T... params) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
-            asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
-        else
-            asyncTask.execute(params);
-    }
-
     @SafeVarargs
-    public static <T> void StartAsyncTask(AsyncTask asyncTask, Void... params) {
+    public static <Params,Progress,Result> void StartAsyncTask(AsyncTask<Params,Progress,Result> asyncTask, Params... params) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
             asyncTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, params);
         else
