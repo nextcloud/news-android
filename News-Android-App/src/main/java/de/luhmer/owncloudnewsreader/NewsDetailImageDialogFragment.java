nextcloud/news-android

commit 1f1b06f96ba97e52ae6bc5086e2db78d85c298e8

Fix wrong resource usage

News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/NewsReaderListDialogFragment.java
@@ -94,8 +94,8 @@ public class NewsReaderListDialogFragment extends DialogFragment{
 
         int style = DialogFragment.STYLE_NO_TITLE;
         int theme = ThemeChooser.isDarkTheme(getActivity())
-                ? R.style.FloatingDialog
-                : R.style.FloatingDialogLight;
+                ? R.style.Theme_Material_Dialog_Floating
+                : R.style.Theme_Material_Light_Dialog_Floating;
         setStyle(style, theme);
     }
 

commit 6c32707bbbff016640e0ebe8009514d2762be1fb

Fix issue caused by 1f1b06f96ba97e52ae6bc5086e2db78d85c298e8

News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/NewsDetailImageDialogFragment.java
@@ -153,8 +153,8 @@ public class NewsDetailImageDialogFragment extends DialogFragment {
 
         int style = DialogFragment.STYLE_NO_TITLE;
         int theme = ThemeChooser.isDarkTheme(getActivity())
-                ? R.style.Theme_Material_Dialog_Floating
-                : R.style.Theme_Material_Light_Dialog_Floating;
+                ? R.style.FloatingDialog
+                : R.style.FloatingDialogLight;
         setStyle(style, theme);
     }
 

News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/NewsReaderListDialogFragment.java
@@ -94,8 +94,8 @@ public class NewsReaderListDialogFragment extends DialogFragment{
 
         int style = DialogFragment.STYLE_NO_TITLE;
         int theme = ThemeChooser.isDarkTheme(getActivity())
-                ? R.style.Theme_Material_Dialog_Floating
-                : R.style.Theme_Material_Light_Dialog_Floating;
+                ? R.style.FloatingDialog
+                : R.style.FloatingDialogLight;
         setStyle(style, theme);
     }
 