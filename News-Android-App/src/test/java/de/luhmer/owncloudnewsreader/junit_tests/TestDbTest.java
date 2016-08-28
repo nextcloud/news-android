07e0c50a02f99674c1723d1f70d2f08ca8e87b82
David Development <david-dev@live.de>
2016-08-28 23:29:39 +0200

    Update

--- News-Android-App/src/test/java/de/luhmer/owncloudnewsreader/junit_tests/TestDbTest.java
+++ News-Android-App/src/test/java/de/luhmer/owncloudnewsreader/junit_tests/TestDbTest.java
@@ -5,7 +5,6 @@ import android.database.Cursor;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.Stopwatch;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricGradleTestRunner;
