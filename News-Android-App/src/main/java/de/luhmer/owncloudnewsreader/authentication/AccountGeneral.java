6510263a503bbdd25a5413f0e8690afeb1e27c05
Stefan Niedermann <stefan.niedermann@googlemail.com>
2016-12-14 22:10:49 +0100

    Nextcloud Theme (#541)
    - Changed strings from ownCloud to neutral "server" where possible
    - Changed strings from ownCloud to Nextcloud in values.xml

--- News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/authentication/AccountGeneral.java
+++ News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/authentication/AccountGeneral.java
@@ -16,9 +16,9 @@ public class AccountGeneral {
 	 * Auth token types
 	 */
 	public static final String AUTHTOKEN_TYPE_READ_ONLY = "Read only";
-	public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an ownCloud News account";
+	public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an Nextcloud News account";
 
 	public static final String AUTHTOKEN_TYPE_FULL_ACCESS = "Full access";
-	public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an ownCloud News account";
+	public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an Nextcloud News account";
 
 }
