42a0230df3a88a580d231c32a26cf75139e0926f
David Luhmer <david-dev@live.de>
2019-11-27 20:49:36 +0100

    use top navigation bar again

--- News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/NewsDetailActivity.java
+++ News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/NewsDetailActivity.java
@@ -26,27 +26,20 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
-import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.text.Html;
 import android.util.Log;
 import android.util.SparseArray;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.webkit.WebView;
-import android.widget.ImageButton;
 import android.widget.ProgressBar;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.appcompat.widget.Toolbar;
 import androidx.browser.customtabs.CustomTabsIntent;
 import androidx.core.content.ContextCompat;
@@ -56,8 +49,6 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
-
-import com.google.android.material.bottomappbar.BottomAppBar;
 
 import java.lang.ref.WeakReference;
 import java.util.HashSet;
@@ -71,12 +62,10 @@ import butterknife.ButterKnife;
 import de.luhmer.owncloudnewsreader.database.DatabaseConnectionOrm;
 import de.luhmer.owncloudnewsreader.database.DatabaseConnectionOrm.SORT_DIRECTION;
 import de.luhmer.owncloudnewsreader.database.model.RssItem;
-import de.luhmer.owncloudnewsreader.helper.ThemeChooser;
 import de.luhmer.owncloudnewsreader.helper.ThemeUtils;
 import de.luhmer.owncloudnewsreader.model.PodcastItem;
 import de.luhmer.owncloudnewsreader.model.TTSItem;
 import de.luhmer.owncloudnewsreader.widget.WidgetProvider;
-import static de.luhmer.owncloudnewsreader.helper.ThemeChooser.THEME;
 
 
 public class NewsDetailActivity extends PodcastFragmentActivity {
@@ -93,8 +82,8 @@ public class NewsDetailActivity extends PodcastFragmentActivity {
 	 * {@link FragmentStatePagerAdapter}.
 	 */
 	private SectionsPagerAdapter mSectionsPagerAdapter;
-    //protected @BindView(R.id.toolbar) Toolbar toolbar;
-	protected @BindView(R.id.bottomAppBar) BottomAppBar bottomAppBar;
+    protected @BindView(R.id.toolbar) Toolbar toolbar;
+	// protected @BindView(R.id.bottomAppBar) BottomAppBar bottomAppBar;
 	protected @BindView(R.id.progressIndicator) ProgressBar progressIndicator;
 	//protected @BindView(R.id.btn_disable_incognito) ImageButton mBtnDisableIncognito;
 
@@ -140,12 +129,15 @@ public class NewsDetailActivity extends PodcastFragmentActivity {
 		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 		*/
 
+		/*
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 			getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 		}
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
 			getWindow().setStatusBarColor(Color.WHITE);
 		}
+		*/
+
 
 
 		setContentView(R.layout.activity_news_detail);
@@ -159,13 +151,14 @@ public class NewsDetailActivity extends PodcastFragmentActivity {
 
 		ButterKnife.bind(this);
 
-		/*
-        if (toolbar != null) {
+		if (toolbar != null) {
             setSupportActionBar(toolbar);
-        }*/
+        }
+		/*
 		if (bottomAppBar != null) {
 			setSupportActionBar(bottomAppBar);
 		}
+		*/
 
 
 		//getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
@@ -621,12 +614,14 @@ public class NewsDetailActivity extends PodcastFragmentActivity {
 
 	public void initIncognitoMode() {
 		int color = getResources().getColor(isIncognitoEnabled() ? R.color.material_grey_900 : R.color.colorPrimary);
-		//ThemeUtils.colorizeToolbar(toolbar, color);
-		ThemeUtils.colorizeToolbar(bottomAppBar, color);
+		ThemeUtils.colorizeToolbar(toolbar, color);
+		//ThemeUtils.colorizeToolbar(bottomAppBar, color);
 		//ThemeUtils.changeStatusBarColor(this, color);
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-			getWindow().setNavigationBarColor(getResources().getColor(R.color.material_grey_900));
+			//getWindow().setNavigationBarColor(color);
+			getWindow().setStatusBarColor(color);
 
+			/*
 			switch (ThemeChooser.getSelectedTheme()) {
 				case LIGHT:
 					Log.d(TAG, "initIncognitoMode: LIGHT");
@@ -644,10 +639,12 @@ public class NewsDetailActivity extends PodcastFragmentActivity {
 					getWindow().setStatusBarColor(Color.BLACK);
 					break;
 			}
+			*/
 		}
 
 	}
 
+	/*
 	private void setLightStatusBar(@NonNull View view) {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
 			int flags = view.getSystemUiVisibility(); // get current flag
@@ -663,6 +660,7 @@ public class NewsDetailActivity extends PodcastFragmentActivity {
 			view.setSystemUiVisibility(flags);
 		}
 	}
+	*/
 
 	/**
 	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
