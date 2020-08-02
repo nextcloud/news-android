894c63e75cf94696029acc61dfdfda7956cc73da
David Luhmer <david-dev@live.de>
2020-08-02 09:42:54 +0200

    Cleanup unused imports
    Signed-off-by: David Luhmer <david-dev@live.de>

--- News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/NewsDetailFragment.java
+++ News-Android-App/src/main/java/de/luhmer/owncloudnewsreader/NewsDetailFragment.java
@@ -21,7 +21,6 @@
 
 package de.luhmer.owncloudnewsreader;
 
-import android.animation.ObjectAnimator;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -30,9 +29,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
-import android.view.GestureDetector;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebBackForwardList;
